7dfd8a1d9888a6724329735eb0a3c90685d2a082
Arwin Tio <arwin.tio@getcruise.com>
2022-12-08 03:13:02 -0800

    Fix

--- runners/spark/src/main/java/org/apache/beam/runners/spark/ArwinSparkJob.java
+++ runners/spark/src/main/java/org/apache/beam/runners/spark/ArwinSparkJob.java
@@ -10,7 +10,7 @@ public class ArwinSparkJob {
     public static void main() {
         try {
           String hostname = new BufferedReader(
-            new InputStreamReader(Runtime.getRuntime().exec(new String[]{"hostname", "-I"}).getInputStream()))
+            new InputStreamReader(Runtime.getRuntime().exec(new String[]{"hostname", "-I"}).getInputStream(), Charsets.UTF_8))
            .readLine();
           System.out.println(hostname);
         } catch (IOException e) {
